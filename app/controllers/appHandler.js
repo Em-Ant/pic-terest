9fed5bc61e95656359a0fa7ca0cc9f5a828faf97
em-ant <joshua999x@gmail.com>
2018-04-29 00:24:30 +0200

    add simple infinite scroll

--- app/controllers/appHandler.js
+++ app/controllers/appHandler.js
@@ -14,94 +14,103 @@ function invalidImage(req, res) {
     ownerId: req.user._id,
     url: process.env.APP_URL + 'img/placeholder.png',
     description: 'Invalid Image'
-  })
-  Pic.populate(pic, {path: 'ownerId'}, function(err, pic){
+  });
+  Pic.populate(pic, { path: 'ownerId' }, function(err, pic) {
     res.json(pic);
-  })
+  });
 }
 
-function AppHandler () {
-
-	this.addPic = function (req, res) {
+function AppHandler() {
+  this.addPic = function(req, res) {
     var pic = req.body;
     pic.ownerId = req.user._id;
-    request({url: pic.webUrl, encoding: null}, function(err, response, body){
-      
+    request({ url: pic.webUrl, encoding: null }, function(err, response, body) {
       if (err) return invalidImage(req, res);
       sharp(body)
         .resize(180)
-        .toBuffer(function(err, data, info){
+        .toBuffer(function(err, data, info) {
           if (err) return invalidImage(req, res);
-          
+
           pic.width = info.width;
           pic.height = info.height;
           pic.format = info.format;
           pic.data = data;
           var newPic = new Pic(pic);
           newPic.url = '/img/' + newPic._id + '.' + newPic.format;
-          newPic.save(function (err) {
+          newPic.save(function(err) {
             if (err) return handleError(res, err);
-            Pic.populate(newPic, { path: "ownerId" }, function (err, pic) {
+            Pic.populate(newPic, { path: 'ownerId' }, function(err, pic) {
               if (err) return handleError(res, err);
               pic = pic.toObject();
               pic.data = undefined;
               res.json(pic);
-            })
-          })
+            });
+          });
         });
-    })
-	};
+    });
+  };
 
-  this.getAllPics = function (req, res) {
+  this.getAllPics = function(req, res) {
+    var offset = parseInt(req.query.offset) || 0;
+    var limit = parseInt(req.query.limit) || 20;
     Pic.find()
       .select('-data')
+      .sort({ date: -1 })
+      .skip(offset)
+      .limit(limit)
       .populate('ownerId')
-      .sort({date: -1})
-      .limit(40)
-      .exec(function(err, pics){
-        if(err) {
-          if(err) return handleError(res, err);
+      .exec(function(err, pics) {
+        if (err) {
+          if (err) return handleError(res, err);
         }
         res.json(pics);
       });
   };
 
-  this.getUserPics = function (req, res) {
-    Pic.find({ownerId: req.params.id})
+  this.getUserPics = function(req, res) {
+    Pic.find({ ownerId: req.params.id })
       .populate('ownerId')
-      .sort({date: -1})
-      .exec(function(err, pics){
-        if(err) {
-          if(err) return handleError(res, err);
+      .sort({ date: -1 })
+      .exec(function(err, pics) {
+        if (err) {
+          if (err) return handleError(res, err);
         }
         res.json(pics);
       });
   };
 
-  this.likePic = function (req, res) {
-    Pic.findOneAndUpdate({_id: req.params.id}, {$push: {likers: req.user._id}}, function (err, pic) {
-      if(err) return handleError(res, err);
-      res.json(pic);
-    })
+  this.likePic = function(req, res) {
+    Pic.findOneAndUpdate(
+      { _id: req.params.id },
+      { $push: { likers: req.user._id } },
+      function(err, pic) {
+        if (err) return handleError(res, err);
+        res.json(pic);
+      }
+    );
   };
 
-  this.unlikePic = function (req, res) {
-    Pic.findOneAndUpdate({_id: req.params.id}, {$pull: {likers: req.user._id}}, function (err, pic) {
-      if(err) return handleError(res, err);
-      res.json(pic);
-    })
+  this.unlikePic = function(req, res) {
+    Pic.findOneAndUpdate(
+      { _id: req.params.id },
+      { $pull: { likers: req.user._id } },
+      function(err, pic) {
+        if (err) return handleError(res, err);
+        res.json(pic);
+      }
+    );
   };
 
-  this.deletePic = function (req, res) {
+  this.deletePic = function(req, res) {
     var query = { _id: req.params.id };
     if (!req.user.isAdmin) {
       query.ownerId = req.user._id;
     }
-    Pic.findOneAndRemove({_id: req.params.id}, function(err, pic) {
-      if(err) return handleError(err);
+    Pic.findOneAndRemove({ _id: req.params.id }, function(err, pic) {
+      if (err) return handleError(err);
       res.json(pic);
-    })
-  }
+    });
+  };
 
   this.getImg = function(req, res) {
     var id = req.params.id.split('.')[0];
@@ -109,8 +118,8 @@ function AppHandler () {
       if (err) return handleError(res, err);
       res.contentType('image/' + pic.format);
       res.send(pic.data);
-    })
-  }
+    });
+  };
 }
 
 module.exports = AppHandler;
--- client/components/index.jsx
+++ client/components/index.jsx
@@ -1,8 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Ajax = new (require('../js/ajax-functions.js'));
+var Ajax = new (require('../js/ajax-functions.js'))();
 
-var $ = jQuery = require('jquery');
+var $ = (jQuery = require('jquery'));
 require('../style/index.scss');
 require('bootstrap');
 var Masonry = require('react-masonry-component');
@@ -12,19 +12,43 @@ var Nav = require('./navbar.jsx');
 
 var appUrl = window.location.origin;
 
+function isAllScrollDown() {
+  var s = $(window).scrollTop(),
+    d = $(document).height(),
+    c = $(window).height();
+
+  var scrollPercent = s / (d - c);
+  return scrollPercent === 1;
+}
+
+function setEndScrollListener(cb) {
+  setTimeout(function() {
+    if (isAllScrollDown()) return cb();
+    $(window).on('scroll', function() {
+      if (isAllScrollDown()) cb();
+    });
+  }, 500);
+}
+
+function removeEndScrollListener() {
+  $(window).off('scroll');
+}
+
 var App = React.createClass({
-  componentDidMount: function () {
+  componentDidMount: function() {
     var self = this;
     Ajax.ajaxRequest('get', appUrl + '/api/user', function(user) {
-      if(user.status !== 'unauthenticated') {
+      if (user.status !== 'unauthenticated') {
         self.setState({
           user: user,
-          loggedIn: true
-        })
+          loggedIn: true,
+          pics: []
+        });
       } else {
         self.setState({
-          user: {twitter:{username: 'guest'}},
-          loggedIn: false
+          user: { twitter: { username: 'guest' } },
+          loggedIn: false,
+          pics: []
         });
       }
       self.getAllPics();
@@ -32,88 +56,112 @@ var App = React.createClass({
   },
   getAllPics: function() {
     var self = this;
-    this.setState({pics:[], loading:true});
-    Ajax.ajaxRequest('get', appUrl + '/api/pics', function(data) {
-      self.setState({pics: data, page: 'all', loading: false});
+    var offset = this.state.pics.length;
+    var limit = 20;
+    var picsUrl =
+      appUrl + '/api/pics?limit=' + limit + (offset ? '&offset=' + offset : '');
+    this.setState({ loading: true });
+    removeEndScrollListener();
+    Ajax.ajaxRequest('get', picsUrl, function(data) {
+      var pics = self.state.pics.concat(data);
+      self.setState({ pics: pics, page: 'all', loading: false });
+      if (data.length && data.length === limit)
+        setEndScrollListener(self.getAllPics);
     });
   },
   getUserPicsbyId: function(id, cb) {
-    Ajax.ajaxRequest('get', appUrl + '/api/pics/' + id, function(data) {
-      cb(data);
-    }.bind(this));
+    Ajax.ajaxRequest(
+      'get',
+      appUrl + '/api/pics/' + id,
+      function(data) {
+        cb(data);
+      }.bind(this)
+    );
   },
   getUserPics: function(index) {
-    this.setState({pics:[], loading:true});
+    this.setState({ pics: [], loading: true });
     var id = this.state.pics[index].ownerId._id;
-    this.getUserPicsbyId(id, function(pics) {
-      this.setState({pics: pics, page: 'user', loading: false});
-    }.bind(this));
+    this.getUserPicsbyId(
+      id,
+      function(pics) {
+        this.setState({ pics: pics, page: 'user', loading: false });
+      }.bind(this)
+    );
   },
-  createPic: function (url, desc) {
-    if(!url) return
+  createPic: function(url, desc) {
+    if (!url) return;
     desc = desc || 'a pic by @' + this.state.user.twitter.username;
-    Ajax.ajaxRequest('post', appUrl + '/api/pics',  function(d) {
-      var pics = this.state.pics.map(function(p){return p;});
-      pics.unshift(d);
-      this.setState({pics: pics});
-    }.bind(this), null, {webUrl: url, description: desc})
+    Ajax.ajaxRequest(
+      'post',
+      appUrl + '/api/pics',
+      function(d) {
+        var pics = this.state.pics.map(function(p) {
+          return p;
+        });
+        pics.unshift(d);
+        this.setState({ pics: pics });
+      }.bind(this),
+      null,
+      { webUrl: url, description: desc }
+    );
   },
   likeHandler: function(index) {
     var self = this;
     var id = this.state.pics[index]._id;
-    var liked = (this.state.pics[index].likers.indexOf(this.state.user._id) !== -1)
+    var liked =
+      this.state.pics[index].likers.indexOf(this.state.user._id) !== -1;
     var verb = liked ? 'put' : 'post';
-    this.setState({picLoading: index});
-    Ajax.ajaxRequest(verb, appUrl + '/api/pics/' + id, function(d){
+    this.setState({ picLoading: index });
+    Ajax.ajaxRequest(verb, appUrl + '/api/pics/' + id, function(d) {
       var pics = self.state.pics;
-      if(liked) {
+      if (liked) {
         var i = d.likers.indexOf(self.state.user._id);
-        d.likers.splice(i,1);
+        d.likers.splice(i, 1);
         pics[index].likers = d.likers;
       } else {
         d.likers.push(self.state.user._id);
         pics[index].likers = d.likers;
       }
-      if(!self.state.loading) {
-        self.setState({pics: pics, picLoading: undefined});
+      if (!self.state.loading) {
+        self.setState({ pics: pics, picLoading: undefined });
       }
-    })
+    });
   },
   deletePic: function(index) {
     var self = this;
     var id = this.state.pics[index]._id;
-    Ajax.ajaxRequest('delete', appUrl + '/api/pics/' + id, function(){
+    Ajax.ajaxRequest('delete', appUrl + '/api/pics/' + id, function() {
       var pics = self.state.pics;
-      pics.splice(index,1);
-      self.setState({pics: pics});
-    })
+      pics.splice(index, 1);
+      self.setState({ pics: pics });
+    });
   },
-  getInitialState : function () {
-    return {user: {twitter:{}}, page: 'all', pics:[]}
+  getInitialState: function() {
+    return { user: { twitter: {} }, page: 'all', pics: [] };
   },
   imgReplacer: function(e) {
     e.target.src = appUrl + '/img/placeholder.png';
   },
   idReplacer: function(e) {
-  e.target.src = appUrl + "/img/twitter-egg-icon.png";
+    e.target.src = appUrl + '/img/twitter-egg-icon.png';
   },
-  setPage: function  (page) {
+  setPage: function(page) {
     var self = this;
     switch (page) {
-      case 'all' :
+      case 'all':
         this.getAllPics();
         break;
       case 'myPics':
         var id = this.state.user._id;
         this.getUserPicsbyId(id, function(pics) {
-          self.setState({pics: pics, page: 'myPics'});
+          self.setState({ pics: pics, page: 'myPics' });
         });
         break;
       default:
         break;
     }
   },
-  render: function () {
+  render: function() {
     var hide = this.state.loading ? '' : ' c-hide';
     var self = this;
     var pics = this.state.pics.map(function(p, i) {
@@ -124,24 +172,26 @@ var App = React.createClass({
           height={p.height || 180}
           description={p.description}
           ownerImg={p.ownerId.twitter.imageUrl}
-          likeable={self.state.loggedIn }
-          username={'@'+p.ownerId.twitter.username}
-          getUserPics={self.getUserPics.bind(null,i)}
+          likeable={self.state.loggedIn}
+          username={'@' + p.ownerId.twitter.username}
+          getUserPics={self.getUserPics.bind(null, i)}
           liked={p.likers.indexOf(self.state.user._id) !== -1}
-          deletable={self.state.loggedIn && (
-            self.state.user.isAdmin || (
-            self.state.page === 'myPics' &&
-            p.ownerId._id === self.state.user._id)
-          )}
+          deletable={
+            self.state.loggedIn &&
+            (self.state.user.isAdmin ||
+              (self.state.page === 'myPics' &&
+                p.ownerId._id === self.state.user._id))
+          }
           likes={p.likers.length}
-          like={self.likeHandler.bind(null,i)}
-          delete={self.deletePic.bind(null,i)}
+          like={self.likeHandler.bind(null, i)}
+          delete={self.deletePic.bind(null, i)}
           imgReplacer={self.imgReplacer}
           idReplacer={self.idReplacer}
           loading={self.state.picLoading === i}
-          userPicsDisabled={self.state.picLoading !== undefined}/>
-        )
-      });
+          userPicsDisabled={self.state.picLoading !== undefined}
+        />
+      );
+    });
     return (
       <div>
         <Nav
@@ -149,28 +199,27 @@ var App = React.createClass({
           loggedIn={this.state.loggedIn}
           page={this.state.page}
           setPage={this.setPage}
-          setPageDisabled={self.state.picLoading !== undefined}/>
+          setPageDisabled={self.state.picLoading !== undefined}
+        />
         <div className="container">
-          <div className={'preloader' + hide}>
-            <img src="/img/preloader.gif"/>
-          </div>
           <Masonry
             className={'grid'}
-            options={
-            {
+            options={{
               // options
               itemSelector: '.grid-item',
               columnWidth: 200,
               fitWidth: true
-            }
-          }>
+            }}
+          >
             {pics}
           </Masonry>
+          <div className={'preloader' + hide}>
+            <img src="/img/preloader.gif" />
+          </div>
         </div>
       </div>
-
-    )
+    );
   }
 });
 
-ReactDOM.render(<App/>, document.getElementById('appView'));
+ReactDOM.render(<App />, document.getElementById('appView'));
